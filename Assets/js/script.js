5851f6e12104b2a21dfb662320bdead2520c0f10
BeKind-Rewind <challenge641@gmail.com>
2022-06-26 22:03:20 -0500

    created the time blocks, added save icon

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,20 @@
 var today =
     $("#currentDate").text(rightNow);
 
-
-
 var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
 console.log(rightNow);
 
+var currentHour = (new Date()).getHours();
+$(".colorcode")
+    .each(function(){
+        var val = parseInt($(this).prop("id"));
+        if(val > currentHour && val < currentHour + 10) {
+            $(this).css("background-color", "green");
+        } else if(val < currentHour && val > currentHour -10) {
+            $(this).css("background-color", "grey");
+        } else if(val === currentHour){
+            $(this).css("background-color", "red");
+        } else {
+            $(this).css("background-color", "red");
+        }
+    });
