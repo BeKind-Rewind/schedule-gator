4927b5d1c4b89641a72f37b6ceff4a152d50ed7b
BeKind-Rewind <challenge641@gmail.com>
2022-06-26 22:23:16 -0500

    added loop for class change past, present, future colorschemes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,17 +4,32 @@ var today =
 var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
 console.log(rightNow);
 
-var currentHour = (new Date()).getHours();
-$(".colorcode")
-    .each(function(){
-        var val = parseInt($(this).prop("id"));
-        if(val > currentHour && val < currentHour + 10) {
-            $(this).css("background-color", "green");
-        } else if(val < currentHour && val > currentHour -10) {
-            $(this).css("background-color", "grey");
-        } else if(val === currentHour){
-            $(this).css("background-color", "red");
-        } else {
-            $(this).css("background-color", "red");
-        }
-    });
+    function hourTracker(){
+        var currentHour = moment()
+            .hour();
+
+        $(".time-block")
+            .each(function(){
+                var blockHour = parseInt($(this)
+                    .attr("id")
+                    .split("hour")[1]);
+                console.log(blockHour, currentHour)
+
+                if (blockHour < currentHour) {
+                    $(this).addClass("past");
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+
+                } else if (blockHour === currentHour) {
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                    $(this).removeClass("future");
+
+                } else {
+                    $(this).removeClass("present");
+                    $(this).removeClass("past");
+                    $(this).addClass("future");
+                }
+            })
+    }
+    hourTracker();
