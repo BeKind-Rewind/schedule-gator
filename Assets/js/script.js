54cf7b5d04adca5563adf0bb97eea621a2e51939
BeKind-Rewind <challenge641@gmail.com>
2022-06-26 22:54:11 -0500

    cleaned up the time-blocks styling issuesremoved script from html and rewrote into js using moment

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,38 @@
-var today =
-    $("#currentDate").text(rightNow);
+// load only after page is ready
+$(document).ready(function () { 
+    // print current date
+    $("#currentDay").text(moment().format("LLLL"));
 
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
+    // event listener for input updates
+    $(".saveBtn").on("click", function (){
+        var text = $(this).siblings(".edit_cont").val();
+        var time = $(this).parent().attr("id");
+        // set, local storage
+        localStorage.setItem(time, text);
+    })
+    // get from local storage
+    $("#0-eight-hundred .edit_cont").val(localStorage.getItem("0-eight-hundred"));
+    $("#0-nine-hundred .edit_cont").val(localStorage.getItem("0-nine-hundred"));
+    $("#ten-hundred .edit_cont").val(localStorage.getItem("ten-hundred"));
+    $("#eleven-hundred .edit_cont").val(localStorage.getItem("eleven-hundred"));
+    $("#twelve-hundred .edit_cont").val(localStorage.getItem("twelve-hundred"));
+    $("#thirteen-hundred .edit_cont").val(localStorage.getItem("thirteen-hundred"));
+    $("#fourteen-hundred .edit_cont").val(localStorage.getItem("fourteen-hundred"));
+    $("#fifteen-hundred .edit_cont").val(localStorage.getItem("fifteen-hundred"));
+    $("#sixteen-hundred .edit_cont").val(localStorage.getItem("sixteen-hundred"));
+    $("#seventeen-hundred .edit_cont").val(localStorage.getItem("seventeen-hundred"));
+
+
 
     function hourTracker(){
         var currentHour = moment()
             .hour();
-
+        // loop over time-blocks and check if class changes (past, present, future)
         $(".time-block")
             .each(function(){
                 var blockHour = parseInt($(this)
                     .attr("id")
                     .split("hour")[1]);
-                console.log(blockHour, currentHour)
 
                 if (blockHour < currentHour) {
                     $(this).addClass("past");
@@ -33,3 +52,4 @@ console.log(rightNow);
             })
     }
     hourTracker();
+})
